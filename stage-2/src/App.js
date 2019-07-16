203756d3cc9ec9d2b17883861cb1ba06de9b50d2
Grant <gwensignak@gmail.com>
2019-07-16 15:52:01 -0600

    part 2 is finished

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -5,7 +5,7 @@ import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
 class App extends Component {
-  constuctor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       products: [],
@@ -16,27 +16,31 @@ class App extends Component {
     this.removeFromCart = this.removeFromCart.bind(this);
     this.navigate = this.navigate.bind(this);
   }
+
   componentDidMount() {
-    axios
-      .get("https://practiceapi.devmountain.com/products/")
-      .then(response => {
-        this.setState({
-          products: response
-        });
+    axios.get("https://practiceapi.devmountain.com/products/").then(response => {
+      this.setState({
+        products: response.data
       });
-  }
-  addToCart(item) {
-    this.setState({
-      cart: item
     });
   }
+
+  addToCart(item) {
+    this.setState({
+      cart: [...this.state.cart, item]
+    });
+    console.log(this.state.cart)
+  }
+
   removeFromCart(index) {
-    let cartCopy = this.state.products.slice();
+    let cartCopy = this.state.cart.slice();
     cartCopy.splice(index, 1);
     this.setState({
       cart: cartCopy
     });
+    console.log(this.state.cart)
   }
+  
   navigate(location) {
     if (location === "cart") {
       this.setState({
@@ -48,6 +52,7 @@ class App extends Component {
       });
     }
   }
+
   render() {
     const { products, cart, showCart } = this.state;
     return (
@@ -57,8 +62,8 @@ class App extends Component {
           {showCart ? (
             <ShoppingCart cart={cart} removeFromCart={this.removeFromCart} />
           ) : (
-            <StoreFront products={products} addToCart={this.addToCart} />
-          )}
+              <StoreFront products={products} addToCart={this.addToCart} />
+            )}
         </div>
       </div>
     );
